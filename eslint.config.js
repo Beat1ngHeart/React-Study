c6b8b09ee6d189b3b4eeaa56e0ad004e390644b1
Beat1ngHeart <dreambear0o0l@gmail.com>
2026-01-06 17:58:05 +0800

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+])
+//这是一个ESLint配置文件，核心作用：定义一套代码规范检查规则，以确保React+TypeScript项目代码质量和一致性，减少低级错误，并强制执行最佳实践。
